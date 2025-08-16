4ca4ac5c4fb2cd20e387b85c32a1967c59330e20
Yono1112 <yuma.ohno.sss@gmail.com>
2025-08-16 17:32:22 +0900

    add writeln macro and enable to display number

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@
 #![feature(offset_of)]
 
 use core::arch::asm;
+use core::fmt;
+use core::fmt::Write;
 use core::mem::{offset_of, size_of};
 use core::panic::PanicInfo;
 use core::ptr::null_mut;
+use core::writeln;
 
 type EfiVoid = u8;
 type EfiHandle = u64;
@@ -309,6 +312,36 @@ fn draw_str_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, s: &str) {
     }
 }
 
+struct VramTextWriter<'a> {
+    vram: &'a mut VramBufferInfo,
+    cursor_x: i64,
+    cursor_y: i64,
+}
+
+impl<'a> VramTextWriter<'a> {
+    fn new(vram: &'a mut VramBufferInfo) -> Self {
+        Self {
+            vram,
+            cursor_x: 0,
+            cursor_y: 0,
+        }
+    }
+}
+impl fmt::Write for VramTextWriter<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            if c == '\n' {
+                self.cursor_y += 16;
+                self.cursor_x = 0;
+                continue;
+            }
+            draw_font_fg(self.vram, self.cursor_x, self.cursor_y, 0xffffff, c);
+            self.cursor_x += 8;
+        }
+        Ok(())
+    }
+}
+
 // ===== エントリポイント =====
 
 #[no_mangle]
@@ -347,6 +380,12 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     }
 
     draw_str_fg(&mut vram, vw / 2, vh / 2, 0xffffff, "Hello, world!");
+
+    let mut w = VramTextWriter::new(&mut vram);
+    // for i in 0..4 {
+    //     writeln!(w, "i = {i}").unwrap();
+    // }
+    writeln!(w, "vw: {vw}, vh: {vh}").unwrap();
 
     hlt_loop();
 }
